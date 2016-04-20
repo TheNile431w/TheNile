064bce2d5896adc63ff37337120e9c822ef8066d
kevin <pkc5055@psu.edu>
2016-04-19 21:05:46 -0700

    Minor fixes

---
+++ viewProfile.php
@@ -0,0 +1,16 @@
+<?php include("htmlHeader.php"); ?>
+    <?php include("htmlTopbar.php"); ?>
+
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title>Profile</title>
+  </head>
+  <body>
+    <center><h1>My Profile</h1></center>
+
+    <!-- Will add a nice modal to specify if its a person or a company. for now only person -->
+
+    </center>
+  </body>
+</html>
